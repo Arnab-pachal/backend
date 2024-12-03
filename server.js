5da972aeba899e571347549869e9d18760129e79
Arnab Pachal <ap.23cs8031@nitdgp.ac.in>
2024-12-03 09:20:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 let port =3001;
-app.listen(port,(req,res)=>{`app is listening on port number${port}`});
+app.listen(port,(req,res)=>{console.log(`app is listening on port number${port}`)});
 app.get("/",(req,res)=>{res.status(200).json("hi This is  server.js")});
